18813055fcf57e20443915aae67c0133686c2579
Devops <109498305+projectaws741@users.noreply.github.com>
2025-10-11 13:48:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,35 @@ import csv
 from datetime import datetime
 import time
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
-PATH = '<YOUR DESIRED PATH>'
+load_dotenv()
+
+PATH = '/app'
 input_path = PATH + '/data/input'
 output_path = PATH + '/data/output'
 
-# Point the directory to Oracle Client files
 time_stamp = datetime.now().strftime('%Y%m%d')
 output_file_name = time_stamp + '_' + 'testOutput.txt'
-cx_Oracle.init_oracle_client(lib_dir='<DIRP ATH TO >instantclient_basic/instantclient_21_3')
+
+cx_Oracle.init_oracle_client(lib_dir='/opt/oracle/instantclient_21_3')
 
 dsn = cx_Oracle.makedsn(
-    host=<hostname>,
-    port=<port_number>,
-    sid=<sid>'
+    host='oracle-db',
+    port=1521,
+    service_name='ORCLPDB1'
 )
 connection = cx_Oracle.connect(
-    user=<USER_NAME>,
-    password=<PASSWORD>,
+    user=os.getenv('ORACLE_USER'),
+    password=os.getenv('ORACLE_PASSWORD'),
     dsn=dsn
 )
 
 query = """ SELECT * FROM C_DUNS_V """
 
-df_mda = pd.read_sql(query,con=connection)
+df_mda = pd.read_sql(query, con=connection)
 
-df_mda.to_csv(output_path+'/'+output_file_name, sep="\t")
+df_mda.to_csv(output_path + '/' + output_file_name, sep="\t")
+
+connection.close()
